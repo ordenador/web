0a1c21d2540d6b6641b2c93f258ac66fccc20801
ordenador <ordenador.cl@gmail.com>
2013-01-29 02:05:17 -0300

    test

--- pushwindows.bat
+++ pushwindows.bat
@@ -1,12 +1,15 @@
 @ECHO OFF
 
+SET /P COMENTARIO=Comentario: 
+REM if not defined COMENTARIO (COMENTARIO="Commit automatico (pushwindows.bat): %fvar%")
+
 for /f "delims=" %%a in ('date/t') do @set mydate=%%a 
 for /f "delims=" %%a in ('time/t') do @set mytime=%%a 
 set fvar=%mydate%%mytime% 
 
 "%ProgramFiles(x86)%\Git\bin\git" add .
-"%ProgramFiles(x86)%\Git\bin\git" commit -a -m "Commit automatico (pushwindows.bat): %fvar%" 
-"%ProgramFiles(x86)%\Git\bin\git" push heroku master
-"%ProgramFiles(x86)%\Git\bin\git" push git@github.com:ordenador/web.git
+"%ProgramFiles(x86)%\Git\bin\git" commit -a -m "%COMENTARIO%" 
+REM "%ProgramFiles(x86)%\Git\bin\git" push heroku master
+REM "%ProgramFiles(x86)%\Git\bin\git" push git@github.com:ordenador/web.git
 
 
