f80a63216de201c9001404b2f132bac2c637da23
ordenador <ordenador.cl@gmail.com>
2013-01-29 01:48:33 -0300

    Commit automatico (pushwindows.bat): 29-01-2013  01:48

--- pushwindows.bat
+++ pushwindows.bat
@@ -1,16 +1,12 @@
 @ECHO OFF
 
-REM get date and time 
 for /f "delims=" %%a in ('date/t') do @set mydate=%%a 
 for /f "delims=" %%a in ('time/t') do @set mytime=%%a 
 set fvar=%mydate%%mytime% 
 
-REM add all new files 
-"C:\Program Files (x86)\Git\bin\git" add .
-"C:\Program Files (x86)\Git\bin\git" commit -a -m "Commit automatico (pushwindows.bat): %fvar%" 
+"%ProgramFiles(x86)%\Git\bin\git" add .
+"%ProgramFiles(x86)%\Git\bin\git" commit -a -m "Commit automatico (pushwindows.bat): %fvar%" 
+"%ProgramFiles(x86)%\Git\bin\git" push
 
-"C:\Program Files (x86)\Git\bin\git" push
-REM git push heroku master
-REM git push git@github.com:ordenador/web.git
 
 
