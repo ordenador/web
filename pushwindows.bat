f3286dc4aaec479bde691ed682f178fe76c074ce
ordenador <ordenador.cl@gmail.com>
2013-01-29 01:43:28 -0300

    Commit automatico (pushwindows.bat): 29-01-2013  01:43

--- pushwindows.bat
+++ pushwindows.bat
@@ -6,7 +6,7 @@ for /f "delims=" %%a in ('time/t') do @set mytime=%%a
 set fvar=%mydate%%mytime% 
 
 REM add all new files 
-git add . 
+start git add *
 git commit -a -m "Commit automatico (pushwindows.bat): %fvar%" 
 
 git push
