d888392a9d0fb8ac0578f96258e109dbc76ea6c4
ordenador <ordenador.cl@gmail.com>
2013-01-29 02:47:57 -0300

    test

--- pushwindows.bat
+++ pushwindows.bat
@@ -1,13 +1,17 @@
-@ECHO ON
-set COMENTARIO=
-SET /P COMENTARIO=Comentario: 
-if o%COMENTARIO%o==oo (set COMENTARIO="Commit automatico (pushwindows.bat): %fvar%")
+@ECHO OFF
+REM Obtener fecha y hora
 for /f "delims=" %%a in ('date/t') do @set mydate=%%a 
 for /f "delims=" %%a in ('time/t') do @set mytime=%%a 
 set fvar=%mydate%%mytime% 
 
+REM Comentario manual o dejar en blanco para mensaje por defecto
+SET COMENTARIO=
+SET /P COMENTARIO=Comentario del commit: 
+if o%COMENTARIO%o==oo (set COMENTARIO="Commit automatico (pushwindows.bat): %fvar%")
+
+REM Comandos git
 "%ProgramFiles(x86)%\Git\bin\git" add .
 "%ProgramFiles(x86)%\Git\bin\git" commit -a -m "%COMENTARIO%" 
-REM "%ProgramFiles(x86)%\Git\bin\git" push heroku master
-REM "%ProgramFiles(x86)%\Git\bin\git" push git@github.com:ordenador/web.git
+"%ProgramFiles(x86)%\Git\bin\git" push heroku master
+"%ProgramFiles(x86)%\Git\bin\git" push git@github.com:ordenador/web.git
 
