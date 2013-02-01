5b80c0e010b50ce0a2930f7be8628d8a2f3a09e9
ordenador <ordenador.cl@gmail.com>
2013-01-31 21:10:01 -0300

    r&r

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = 'f35bdacfce843056d6dbe33be594222bb67139af05786ad8cd8883be7572c27cf63c6f721678884328405937b612eff697ed7298d2dc9043ecc09f3c1d2e80fe'
---
+++ start.bat
@@ -0,0 +1,1 @@
+start bundle exec ruby web.rb -p 80
